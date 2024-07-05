b0b3706a411d09ea5e7afd20b26e0f6d765fbf56
Jonathan Hingorani <jonathanhingorani@gmail.com>
2024-07-05 12:00:31 +0200

    attempted

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,54 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import { useState } from "react";
 import ExponentTwo from "./components/ExponentTwo";
 import ExponentThree from "./components/ExponentThree";
 import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
 import ExponentSix from "./components/ExponentSix";
+import Exponent from './components/Exponent'
 
 
 function App () {
+  const [count, setCount] = useState(0);
+  
+  const decrement = () => setCount((prevCount)) => prevCount -1;
+  const increment = () => setCount((prevCount)) => prevCount +1;
+  
   return (
     <div className="App">
       <h2><em>Counter</em></h2>
   
-      <Counter/>
+      <Counter
+        count={count}
+        decrement={decrement}
+        increment={increment}
+      />
 
       <br />
       <h2><em>Exponents</em></h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+        <ExponentTwo
+          num={count}
+          exponent={2} 
+          />
+        <ExponentThree
+          num={count}
+          exponent={3} 
+        />
+        <ExponentFour
+          num={count}
+          exponent={4} 
+        />
+        <ExponentFive
+          num={count}
+          exponent={5} 
+        />
+        <ExponentSix
+          num={count}
+          exponent={6} 
+        />
       </div>
     </div>
   );
